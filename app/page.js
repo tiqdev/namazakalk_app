4fd8489ef0514c520b5f4fb9a27425d5c5dfc9ea
Tarık KAYA <tiqqq24@gmail.com>
2023-07-29 09:21:13 +0300

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
+  const [isDone, setIsDone] = useState(false);
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  useEffect(() => {
+    const data = localStorage.getItem("sabahnamazi");
+    if (data) {
+      const { isDone, date } = JSON.parse(data);
+      const now = new Date();
+      if (
+        now.getDate() === new Date(date).getDate() &&
+        now.getMonth() === new Date(date).getMonth() &&
+        now.getFullYear() === new Date(date).getFullYear()
+      ) {
+        setIsDone(isDone);
+        setTimeout(() => {
+          confettiPlay();
+        }, 1000);
+      }
+    }
+    setPageLoaded(true);
+  }, []);
+
+  const handleClicked = () => {
+    localStorage.setItem(
+      "sabahnamazi",
+      JSON.stringify({ isDone: true, date: new Date() })
+    );
+    setIsDone(true);
+    confettiPlay();
+  };
+
+  //confetti patlatma fonksiyonu
+  const confettiPlay = () => {
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: {
+        x: 0.5,
+        y: 0.5,
+      },
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex h-screen flex-col items-center justify-between py-24 px-12 bg-primary">
+      <h1 className="text-4xl font-bold text-center text-secondary">
+        Namaza Kalktın Mı?
+      </h1>
+
+      <AnimatePresence>
+        {!pageLoaded ? (
+          "Yükleniyor..."
+        ) : isDone ? (
+          <div className="flex flex-col flex-1 items-center justify-center">
+            <motion.img
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1.1, opacity: 1 }}
+              transition={{ duration: 1, ease: "backInOut" }}
+              src="/smile.png"
+              alt="emoji"
+              className="w-32 h-32 drop-shadow-md"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, ease: "backInOut" }}
+              className="text-2xl font-bold text-center text-secondary mt-[20px]"
+            >
+              Çok Şükür <br /> Kalktım
+            </motion.h2>
+          </div>
+        ) : (
+          <div className="flex flex-col flex-1 items-center justify-center space-y-[20px]">
+            <motion.img
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1.1, opacity: 1 }}
+              transition={{ duration: 1, ease: "backInOut" }}
+              src="/sleepy.png"
+              alt="emoji"
+              className="w-32 h-32"
+            />
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, ease: "backInOut" }}
+              onClick={handleClicked}
+              className="px-[12px] py-[16px] text-xl font-bold text-center text-primary bg-secondary rounded-[24px]"
+            >
+              Kalkıyorum İnşallah
+            </motion.button>
+          </div>
+        )}
+      </AnimatePresence>
     </main>
-  )
+  );
 }
