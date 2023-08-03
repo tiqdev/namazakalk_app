2f47eaf5bf126aaa5d94c7d145516ecab2d22784
Tarık KAYA <tiqqq24@gmail.com>
2023-08-04 00:38:46 +0300

    og image ve twitter image denendi tailwind og image denendi

--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -1,5 +1,13 @@
 import { ImageResponse } from 'next/server'
 
+//export twitter image 
+export const config = {
+    twitter: {
+        card: 'summary_large_image',
+    },
+}
+
+
 // Route segment config
 export const runtime = 'edge'
 
@@ -10,7 +18,6 @@ export const size = {
     height: 630,
 }
 
-
 export const contentType = 'image/png'
 
 // Image generation
@@ -18,18 +25,23 @@ export default function Image() {
     return new ImageResponse(
         (
             // ImageResponse JSX element
-            <div
-                style={{
-                    fontSize: 128,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                About tiqdev
+            <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+                <div tw="bg-gray-50 flex w-full">
+                    <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+                        <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+                            <span>Ready to dive in?</span>
+                            <span tw="text-indigo-600">Start your free trial today.</span>
+                        </h2>
+                        <div tw="mt-8 flex md:mt-0">
+                            <div tw="flex rounded-md shadow">
+                                <a tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-bold text-white">Get started</a>
+                            </div>
+                            <div tw="ml-3 flex rounded-md shadow">
+                                <a tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-bold text-indigo-600">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         ),
         // ImageResponse options
@@ -37,6 +49,9 @@ export default function Image() {
             // For convenience, we can re-use the exported opengraph-image
             // size config to also set the ImageResponse's width and height.
             ...size,
+            //add twitter image config
+
+
         }
     )
 }
