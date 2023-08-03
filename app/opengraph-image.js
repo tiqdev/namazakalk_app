2ecca373114cda5cf0d58f421fe009779cd454e1
Tarık KAYA <tiqqq24@gmail.com>
2023-08-03 23:46:28 +0300

    og image deneme 1

---
+++ app/opengraph-image.js
@@ -0,0 +1,46 @@
+import { ImageResponse } from 'next/server'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'About Acme'
+export const size = {
+    width: 1200,
+    height: 630,
+}
+
+// Font
+const interSemiBold = fetch(
+    new URL('./Inter-SemiBold.ttf', import.meta.url)
+).then((res) => res.arrayBuffer())
+
+export const contentType = 'image/png'
+
+// Image generation
+export default function Image() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 128,
+                    background: 'white',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                About tiqdev
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
