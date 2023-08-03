7575e27b511ed1d9c5aec126d0fe063b184da57b
Tarık KAYA <tiqqq24@gmail.com>
2023-08-03 23:58:00 +0300

    og denemesi 3

--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -4,7 +4,7 @@ import { ImageResponse } from 'next/server'
 export const runtime = 'edge'
 
 // Image metadata
-export const alt = 'About Acme'
+export const alt = 'About tiqdev'
 export const size = {
     width: 1200,
     height: 630,
