74449a6884ad38a2e15486d88eb8fee2ced0a6d8
Tarık KAYA <tiqqq24@gmail.com>
2023-08-04 01:34:19 +0300

    özelliklker eklendi

--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -1,5 +1,14 @@
 import { ImageResponse } from 'next/server'
 
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
+
+
 //export twitter image 
 export const config = {
     twitter: {
